74dff861034aa381fedd5ba01da46b09992b4ef0
Mehdi Qaos <mehdi.qaos@gmail.com>
2022-09-08 13:20:34 +0100

    modification de challenge 12

--- challenge12.c
+++ challenge12.c
@@ -1,12 +1,30 @@
 #include<stdio.h>
 
 int main() {
-    int nombre = 234;
+    int nombre = 800;
+
+    // solution sans creer un nouveau variable
     printf("le nombre de 3 chifres: %d\n", nombre);
     printf("%d", nombre % 10);
     nombre /= 10;
     printf("%d", nombre % 10);
     nombre /= 10;
     printf("%d\n", nombre % 10);
+
+    /* autre solution
+    int inverse = 0;
+    int temp = nombre;
+    inverse += temp % 10;
+    temp /= 10;
+    inverse *= 10;
+    inverse += temp % 10;
+    temp /= 10;
+    inverse *= 10;
+    inverse += temp % 10;
+
+    printf("le nombre de 3 chifres: %d\n", nombre);
+    printf("l'inverse : %d\n", inverse);
+    */
+
     return 0;
 }
