6ea6ea444a168a341c30ec400f5bd2b1a859c287
Mehdi Qaos <mehdi.qaos@gmail.com>
2022-09-08 21:05:29 +0100

    added challenge 3 boucles

---
+++ boucles/challenge3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+  int a, b, somme;
+  printf("Entrer le premier nombre entier: ");
+  scanf("%d", &a);
+  printf("Entrer le deuxieme nombre entier: ");
+  scanf("%d", &b);
+
+  somme = a + b;
+
+  if(a == b)
+    printf("%d\n", 3 * somme);
+  else
+    printf("%d\n", somme);
+  return 0;
+}
