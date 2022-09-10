36b3c2786b300fd51c48a1388f7eadb047b24b37
Mehdi Qaos <mehdi.qaos@gmail.com>
2022-09-10 20:15:55 +0100

    .gitignore modifications

--- boucles/test.c
+++ boucles/test.c
@@ -1,7 +1,10 @@
-
 #include <stdio.h>
 
 int main () {
-  printf("%d\n", ' ');
+  char ch1[1000], ch2[1000];
+  gets(ch1);
+  gets(ch2);
+  printf("'%s'\n", ch1);
+  printf("'%s'\n", ch2);
   return 0;
 }
