68ba809f99a05fc68db74cefcc2458e0c4c2daf5
Mehdi Qaos <mehdi.qaos@gmail.com>
2022-09-12 11:23:16 +0100

    modification du challenge 11 fonctions

--- fonctions/challenge11.c
+++ fonctions/challenge11.c
@@ -9,6 +9,13 @@ bool is_leap_year(int year) {
 }
 
 int main() {
+    int annee;
+    printf("Entrer une annee entre 1800 et 10000: ");
+    scanf("%d", &annee);
+
+    printf("%d %s une annee bissextiles\n", annee, is_leap_year(annee) == true ? "est" : "n'est pas");
+    
+    /* test de la fonction is_leap_year
     int leap_years[10] = { 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020 };
     int not_leap_years[10] = { 1805, 1921, 1941, 1949, 1953, 2003, 2018, 2019, 2021, 2022 };
 
@@ -19,5 +26,6 @@ int main() {
     printf("\nnot leap years: \n");
     for(int i=0; i<10; ++i)
         printf("%d is leap year: %s\n", not_leap_years[i], is_leap_year(not_leap_years[i]) == true ? "True": "False");
+    */
     return 0;
 }
